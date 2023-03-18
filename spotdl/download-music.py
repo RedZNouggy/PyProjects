619e1aa9315e68d67205d854e00be4686f42d8a1
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2023-03-19 00:57:51 +0100

    Update download-music.py

--- spotdl/download-music.py
+++ spotdl/download-music.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-# Made by Samuel PAGES 19/03/2023
 import re
 import os
 import argparse
@@ -10,9 +9,12 @@ from urllib.parse import urlparse
 import shutil
 import subprocess
 
+temp_path =  {ChangePath}
+final_path = {ChangePath}
+
 # Retrieving the Spotify album link from the command line arguments
-parser = argparse.ArgumentParser(description='Download Spotify album as MP3')
-parser.add_argument('album_url', type=str, help='Link of the Spotify album')
+parser = argparse.ArgumentParser(description='Download Spotify album in MP3')
+parser.add_argument('album_url', type=str, help='Link of Spotify album')
 args = parser.parse_args()
 album_url = args.album_url
 
@@ -24,24 +26,24 @@ album_name = re.sub(r' - .* by .*', '', title).strip()
 artist_name = re.sub(r'.* - .* by ', '', title).replace(' | Spotify', '').strip()
 
 # Creating a folder with the artist's name
-if not os.path.exists(f'{Change-Path}{artist_name}/'):
-    os.mkdir(f'{Change-Path}{artist_name}/')
+if not os.path.exists(f'{final_path}/{artist_name}/'):
+    os.mkdir(f'{final_path}/{artist_name}/')
 
 # Create a folder with the name of the album
-if not os.path.exists(f'{Change-Path}{artist_name}/{album_name}/'):
-    os.mkdir(f'{Change-Path}{artist_name}/{album_name}/')
+if not os.path.exists(f'{final_path}/{artist_name}/{album_name}/'):
+    os.mkdir(f'{final_path}/{artist_name}/{album_name}/')
 
 # Download the album with spotdl
-subprocess.run(["python3", "-m", "spotdl", f' -o {Change-Path}{artist_name}/{album_name}/', album_url], check=True)
+subprocess.run(["spotdl", "-o", f"{temp_path}" ,album_url], check=True)
 
-dir_path = f"{Change-Path}{artist_name}/{album_name}/"
+dir_path = f"{final_path}/{artist_name}/{album_name}/"
 
 # Move all MP3 files to the destination folder
-for file_name in os.listdir("{Change-Path}"):
+for file_name in os.listdir(f"{temp_path}"):
     if file_name.endswith(".mp3"):
         src_path = os.path.join(os.getcwd(), file_name)
         dst_path = os.path.join(dir_path, file_name)
         shutil.move(src_path, dst_path)
 
-if os.path.exists(f'{Change-Path}.spotdl-cache'):
-    os.remove('{Change-Path}.spotdl-cache')
+if os.path.exists('{temp_path}/.spotdl-cache'):
+    os.remove('{temp_path}/.spotdl-cache')
