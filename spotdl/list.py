d9b30c631e65de5d66fbf6dadbf048878e11584f
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2023-03-19 01:10:50 +0100

    Create list.py

---
+++ spotdl/list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+
+# Checking the number of arguments
+if len(sys.argv) != 2:
+    print("Usage: python3 list.py filename.txt")
+    sys.exit(1)
+
+# Opening the text file in read mode
+filename = sys.argv[1]
+with open(filename, 'r') as f:
+    # Reading the file line by line
+    for line in f:
+        # Removal of spaces at the beginning and end of a line
+        line = line.strip()
+        # Execution of the "download line" command using subprocess
+        subprocess.run(["python3", "/opt/data1To/Create Music/download-music.py", line], check=True)
