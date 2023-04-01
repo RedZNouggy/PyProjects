9e3e6693ca1b5a832423fe1ba1c31e43bb2ede2a
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2023-04-02 01:39:15 +0200

    Update list.py

--- spotdl/list.py
+++ spotdl/list.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import subprocess
+from colorama import Fore, Style
 
 # Checking the number of arguments
 if len(sys.argv) != 2:
     print("Usage: python3 list.py tabl.txt")
     sys.exit(1)
 
+
+# Error message
+def errortext(msg) -> str:
+    '''Display success message on console in green
+    Arguments:
+    msg -- text to be printed
+    '''
+    print(Fore.RED + "[-] " + msg + Style.RESET_ALL)
+
+
 # Opening the text file in read mode
-filename = sys.argv[1]
-with open(filename, 'r') as f:
-    # Reading the file line by line
-    for line in f:
-        # Removal of spaces at the beginning and end of a line
-        line = line.strip()
-        # Execution of the "download line" command using subprocess
-        subprocess.run(["python3", "{ChangePath}download-music.py", line], check=True)
+FILE_NAME = sys.argv[1]
+with open(FILE_NAME, 'r', encoding="utf-8") as f:
+    if os.path.getsize(FILE_NAME) > 0:
+        errortext("Nothing in the txt file")
+    else:
+        # Reading the file line by line
+        for line in f:
+            # Removal of spaces at the beginning and end of a line
+            line = line.strip()
+            # Execution of the "download line" command using subprocess
+            subprocess.run([
+                "python3",
+                "/opt/data2To/download/download_music.py",
+                "--temp-path '{ChangePath}'",
+                "--final-path '{ChangePath}'",
+                line
+            ], check=False)
