ebaa74805b4c6dd12a8fd3930ed0de17431344a1
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2023-04-07 16:52:13 +0200

    Update download-music.py

--- spotdl/download-music.py
+++ spotdl/download-music.py
@@ -3,24 +3,75 @@
 import re
 import os
 import argparse
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 import shutil
 import subprocess
-
-temp_path =  {ChangePath}
-final_path = {ChangePath}
+from colorama import Fore, Style
+import requests
+from bs4 import BeautifulSoup
 
 # Retrieving the Spotify album link from the command line arguments
 parser = argparse.ArgumentParser(description='Download Spotify album in MP3')
-parser.add_argument('album_url', type=str, help='Link of Spotify album')
+parser.add_argument('album_url', type=str,
+                    help='Link of Spotify album')
+parser.add_argument('--temp-path', action="store", type=str,
+                    help='Enter the path where MP3 will be temporary stored')
+parser.add_argument('--final-path', action="store", type=str,
+                    help='Enter the path where MP3 will be definitly stored like '
+                    '"{final_path} + / + artist_name + / + album_name + / + file_name"')
+
 args = parser.parse_args()
 album_url = args.album_url
 
+
+# Info message
+def infotext(msg) -> str:
+    '''Display informational message on console in blue
+    Arguments: the creation date, hash, OS, scope, source, user as a verbose option.
+    msg -- text to be printed
+    '''
+    print(Fore.BLUE + "[i] " + msg + Style.RESET_ALL)
+
+
+# Error message
+def errortext(msg) -> str:
+    '''Display success message on console in green
+    Arguments:
+    msg -- text to be printed
+    '''
+    print(Fore.RED + "[-] " + msg + Style.RESET_ALL)
+
+
+# Warning message
+def warntext(msg) -> str:
+    '''Display success message on console in green
+    Arguments:
+    msg -- text to be printed
+    '''
+    print(Fore.YELLOW + "[~] " + msg + Style.RESET_ALL)
+
+
+# Success message
+def successtext(msg) -> str:
+    '''Display success message on console in green
+    Arguments:
+    msg -- text to be printed
+    '''
+    print(Fore.GREEN + "[+] " + msg + Style.RESET_ALL)
+
+
+if not args.temp_path:
+    errortext("You have to use --temp-path")
+else:
+    temp_path = args.temp_path
+if not args.final_path:
+    errortext("You have to use --final-path")
+else:
+    final_path = args.final_path
+
 # Retrieve artist name and album name from Spotify page title
-html = requests.get(album_url).text
+html = requests.get(album_url, timeout=300).text
 soup = BeautifulSoup(html, 'html.parser')
+
 title = soup.find('title').text
 album_name = re.sub(r' - .* by .*', '', title).strip()
 artist_name = re.sub(r'.* - .* by ', '', title).replace(' | Spotify', '').strip()
@@ -34,7 +85,7 @@ if not os.path.exists(f'{final_path}/{artist_name}/{album_name}/'):
     os.mkdir(f'{final_path}/{artist_name}/{album_name}/')
 
 # Download the album with spotdl
-subprocess.run(["spotdl", "-o", f"{temp_path}" ,album_url], check=True)
+subprocess.run(["spotdl", "--output", temp_path ,album_url], check=True)
 
 dir_path = f"{final_path}/{artist_name}/{album_name}/"
 
